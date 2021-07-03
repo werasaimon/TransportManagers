78f39d490c4d26f457e6a6e9a10ec4fee00e02fb
werasaimon <nicemankit@gmail.com>
2021-07-04 01:34:00 +0300

    Common Structurs

---
+++ Common/help_func.hpp
@@ -0,0 +1,11 @@
+#ifndef HELP_FUNC_HPP
+#define HELP_FUNC_HPP
+
+unsigned constexpr const_hash(char const *input)
+{
+    return *input ?
+        static_cast<unsigned int>(*input) + 33 * const_hash(input + 1) :
+        5381;
+}
+
+#endif // HELP_FUNC_HPP
