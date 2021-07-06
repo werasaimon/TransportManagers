a6eeee794da1903190f3ad12e5cf16660c4f2ebe
werasaimon <nicemankit@gmail.com>
2021-07-06 03:58:05 +0300

    Common ver.0.01

--- Common/help_func.hpp
+++ Common/help_func.hpp
@@ -8,4 +8,7 @@ unsigned constexpr const_hash(char const *input)
         5381;
 }
 
+
+
+
 #endif // HELP_FUNC_HPP
---
+++ Common/order.h
@@ -0,0 +1,18 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <QString>
+
+class Order
+{
+public:
+    Order();
+
+     int ID;
+     QString Text;
+     QString Data;
+     QString Username;
+};
+
+
+#endif // ORDER_H
